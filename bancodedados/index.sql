fc6b0f5d7911372a243a8e69293c5248c872e321
Maria Yasmin <mariayasmim.wtf@gmail.com>
2025-08-11 19:47:50 -0300

    atualizaçao banco de dados

--- bancodedados/index.sql
+++ bancodedados/index.sql
@@ -6,3 +6,15 @@ CREATE TABLE usuarios (
     data_nascimento DATE,
     genero VARCHAR(20)
 );
+
+CREATE TABLE emocoes (
+    id_emocao SERIAL PRIMARY KEY,
+    id_usuario SERIAL,
+    data_registro DATE,
+    alegria INT,
+    tristeza INT,
+    raiva INT,
+    medo INT,
+    ansiedade INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+);
